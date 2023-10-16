a94c1741cb29bfee40890d0a10516aa5f2390c45
officialeric <officialeric994@gmail.com>
2023-10-16 12:10:23 +0300

    form in compose page added

--- resources/views/student/compose.blade.php
+++ resources/views/student/compose.blade.php
@@ -24,8 +24,73 @@
         </nav>
     </header>
 
-    <section class="flex justify-center text-center bg-white dark:bg-gray-900 mt-12">
-        <h3>A form will be here</h3>
+    <section class="flex justify-center bg-white dark:bg-gray-900 mt-12">
+        
+
+<form>
+    <div class="space-y-12">
+      <div class="border-b border-gray-900/10 pb-12">
+        <h2 class="text-base font-semibold leading-7 text-gray-900">Compose Complaint</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
+  
+        <div class="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div class="sm:col-span-4">
+            <label for="regno" class="block text-sm font-medium leading-6 text-gray-900">Registration Number</label>
+            <div class="mt-2">
+              <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                {{-- <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">Reg_No/</span> --}}
+                <input type="number" name="regno" id="regno" autocomplete="regno" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="220229*****">
+              </div>
+            </div>
+          </div>
+  
+          <div class="sm:col-span-4">
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+            <div class="mt-2">
+              <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            </div>
+            
+          </div>
+
+          <div class="sm:col-span-3">
+            <label for="category" class="block text-sm font-medium leading-6 text-gray-900">Choose Category</label>
+            <p class="mt-1 text-sm leading-6 text-gray-600">Choose which category you want to compose a complaint.</p>
+
+            <div class="mt-2">
+              <select id="category" name="category" autocomplete="category-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                <option>Examination</option>
+                <option>IPT issues</option>
+                <option>Registration</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="col-span-full">
+            <label for="about" class="block text-sm font-medium leading-6 text-gray-900">About</label>
+            <div class="mt-2">
+              <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            </div>
+            <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences to describe your complaint.</p>
+          </div>
+  
+        </div>
+      </div>
+      </div>
+{{--   
+      <div class="border-b border-gray-900/10 pb-12">
+       
+        
+      </div> --}}
+    </div>
+  
+    <div class="mt-2 flex items-center justify-end gap-x-6">
+      <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+      <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+    </div>
+  </form>
+  
+
     </section>
 
     
